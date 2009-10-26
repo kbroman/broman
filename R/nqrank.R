cc4ae0e2f30f0cc9624227251bd9c117278fe2ff
Karl Broman <kbroman@gmail.com>
2009-10-26 13:12:40 -0500

    Slight change to nqrank, to preserve mean and SD.

--- R/nqrank.R
+++ R/nqrank.R
@@ -3,7 +3,7 @@
 # nqrank.R
 #
 # copyright (c) 2001-9, Karl W Broman
-# last modified Aug, 2009
+# last modified Oct, 2009
 # first written Nov, 2001
 #
 #     This program is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@
 ######################################################################
 #
 # nqrank: Convert a set of quantitative values to the corresponding
-#         normal quantiles
+#         normal quantiles (preserving mean and SD)
 #
 ######################################################################
 
@@ -34,13 +34,18 @@ nqrank <-
 function(x, jitter=FALSE)
 {
   y <- x[!is.na(x)]
+  themean <- mean(y, na.rm=TRUE)
+  thesd <- sd(y, na.rm=TRUE)
+
   y[y == Inf] <- max(y[y<Inf])+10
   y[y == -Inf] <- min(y[y > -Inf]) + 10
   if(jitter)
     y <- rank(y+runif(length(y))/(sd(y)*10^8))
   else y <- rank(y)
+
   x[!is.na(x)] <- qnorm((y-0.5)/length(y))
-  x
+
+  x*thesd/sd(x, na.rm=TRUE)-mean(x,na.rm=TRUE)+themean
 }
 
 # end of nqrank.R
