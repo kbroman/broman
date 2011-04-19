1bf63e2ae2d4fdefd29eeeabfeca4c8531a9379a
Karl Broman <kbroman@gmail.com>
2011-04-19 18:48:54 -0500

    Add bromanversion() function

---
+++ R/util.R
@@ -0,0 +1,36 @@
+######################################################################
+#
+# util.R
+#
+# copyright (c) 2011, Karl W Broman
+# last modified Apr, 2011
+# first written Apr, 2011
+#
+#     This program is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public License,
+#     version 3, as published by the Free Software Foundation.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but without any warranty; without even the implied warranty of
+#     merchantability or fitness for a particular purpose.  See the GNU
+#     General Public License, version 3, for more details.
+# 
+#     A copy of the GNU General Public License, version 3, is available
+#     at http://www.r-project.org/Licenses/GPL-3
+# 
+# Part of the R/broman package
+# Contains: bromanversion
+#
+######################################################################
+
+######################################################################
+# print the installed version of R/broman
+######################################################################
+bromanversion <-
+function()
+{
+  u <- strsplit(library(help=broman)[[3]][[1]][2]," ")[[1]]
+  u[length(u)]
+}
+
+# end of util.R
