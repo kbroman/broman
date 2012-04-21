4071ffcda80246dc54e7740a7bbc9332681a01be
Karl Broman <kbroman@gmail.com>
2012-04-21 01:11:27 -0500

    In manyboxplot(), change default for x-axis labelling

--- R/manyboxplot.R
+++ R/manyboxplot.R
@@ -44,14 +44,20 @@ function(x, probs=c(0.05, 0.1, 0.25), dotcol="blue",
 
   # this is to deal with varying inputs (did "..." include xaxs or not?)
   manyboxplot.sub <-
-  function(xqu, dotcol, xlab="", xat=NA, xaxs="i", ylab="quantiles",
-           xlim=c(0, ncol(xqu)+1), linecol, type="p",
-           lwd=2, lty=1, pch=16, ylim=range(xqu), ...)
+  function(xqu, dotcol, xlab="", xaxs="i", ylab="quantiles",
+           xlim=c(0.25, ncol(xqu)+.75), linecol, type="p", 
+           xat, lwd=2, lty=1, pch=16, ylim=range(xqu), ...)
   {
     medrow <- (nrow(xqu)-1)/2+1
     ncolx <- ncol(xqu)
+
+    if(missing(xat)) {
+      xat <- pretty(1:ncolx)
+      xat <- c(1, xat[xat > 0])
+    }
+
     grayplot(1:ncolx, xqu[medrow,], col=dotcol, xlab=xlab, type=type,
-             xat=xat, xlim=xlim, xaxs=xaxs, pch=pch, ylim=ylim, ylab=ylab, ...)
+             xlim=xlim, xaxs=xaxs, pch=pch, ylim=ylim, ylab=ylab, xat=xat, ...)
     for(j in 1:(medrow-1)) {
       lines(1:ncolx, xqu[j,], col=linecol[j], lwd=lwd, lty=lty)
       lines(1:ncolx, xqu[nrow(xqu)-j+1,], col=linecol[j], lwd=lwd, lty=lty)
