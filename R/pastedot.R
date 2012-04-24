b78c18e2b25e3a60f66c6b815eda098d904e3e29
Karl Broman <kbroman@gmail.com>
2012-04-23 20:51:18 -0500

    Add function paste., which is just paste(..., sep=".")

---
+++ R/pastedot.R
@@ -0,0 +1,29 @@
+######################################################################
+#
+# pastedot.R
+#
+# copyright (c) 2012, Karl W Broman
+# First written Apr, 2012
+# Last modified Apr, 2012
+#
+#     This program is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public License,
+#     version 3, as published by the Free Software Foundation.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but without any warranty; without even the implied warranty of
+#     merchantability or fitness for a particular purpose.  See the GNU
+#     General Public License, version 3, for more details.
+# 
+#     A copy of the GNU General Public License, version 3, is available
+#     at http://www.r-project.org/Licenses/GPL-3
+# 
+# Part of the R/broman package
+# Contains: pastedot
+#
+######################################################################
+
+
+paste. <- function(...) paste(..., sep=".")
+
+# end of pastedot.R
