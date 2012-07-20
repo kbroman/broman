a636b08172f1265a79e997babeeea4a4623557c3
Karl Broman <kbroman@gmail.com>
2012-07-19 23:14:18 -0500

    Fix bugs in dec2hex; add to examples for hex2dec and dec2hex

--- R/convert2hex.R
+++ R/convert2hex.R
@@ -1,21 +1,21 @@
 ######################################################################
-# convert a number to hex notation
+# convert a number to hex notation (and back)
 #
 #     This program is free software; you can redistribute it and/or
 #     modify it under the terms of the GNU General Public License,
 #     version 3, as published by the Free Software Foundation.
-# 
+#
 #     This program is distributed in the hope that it will be useful,
 #     but without any warranty; without even the implied warranty of
 #     merchantability or fitness for a particular purpose.  See the GNU
 #     General Public License, version 3, for more details.
-# 
+#
 #     A copy of the GNU General Public License, version 3, is available
 #     at http://www.r-project.org/Licenses/GPL-3
-# 
+#
 ######################################################################
 
-dec2hex <- 
+dec2hex <-
 convert2hex <-
 function(d)
 {
@@ -25,14 +25,15 @@ function(d)
       res[i] <- convert2hex(d[i])
     return(res)
   }
-  
-  if(d != round(d)) 
-    stop("d must be an integer")
+
+  if(d != round(d) || d < 0)
+    stop("d must be a non-negative integer")
 
   hex <- c(0:9,LETTERS[1:6])
 
+  if(d < 16) return(hex[d+1])
+
   high <- floor(log(d, 16))
-  if(high < 1) return(hex[d])
 
   res <- ""
   for(i in high:1) {
@@ -40,7 +41,7 @@ function(d)
     res <- paste(res, hex[z + 1], sep="")
     d <- d - z*(16^i)
   }
-  
+
   paste(res, hex[(d %% 16) + 1], sep="")
 }
 
@@ -56,11 +57,14 @@ function(h)
 
   hex <- c(0:9,LETTERS[1:6])
 
+  if(!is.character(h)) h <- as.character(h)
+
   hspl <- rev(unlist(strsplit(h, "")))
   hc <- match(hspl, hex)-1
   if(any(is.na(hc)))
      stop("Invalid characters (", paste(hspl[is.na(hc)], collapse=" "), ") in ", h)
 
   sum(hc * 16^(0:(length(hc)-1)))
-}  
+}
+
 # end of convert2hex.R
