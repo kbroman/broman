66c9a7b5b99f633e9df2a5de432b7ada08ca2aec
Karl Broman <kbroman@gmail.com>
2014-03-14 10:38:30 -0500

    Add makefile argument to make() function

--- R/make.R
+++ R/make.R
@@ -2,13 +2,14 @@
 #'
 #' Run make within a package directory
 #'
-#' @param pkg package package description, can be path or package name.
+#' @param pkg package description, can be path or package name.
 #' See \code{\link[devtools]{as.package}} for more information
+#'
+#' @param makefile File name of makefile
 #'
 #' @param quiet If TRUE suppresses output from this function
 #'
 #' @export
-#' @importFrom devtools as.package 
 #' @return Exit value from \code{\link[base]{system}} with \code{intern=FALSE}
 #'
 #' @examples
@@ -19,13 +20,15 @@
 #' @seealso \code{\link[devtools]{load_all}}
 #' @keywords utilities
 make <-
-function(pkg = ".", quiet=FALSE)
+function(pkg = ".", makefile="Makefile", quiet=FALSE)
 {
-  pkg <- as.package(pkg)
+  pkg <- devtools::as.package(pkg)
 
   if(!quiet) message("Making ", pkg$package)
 
-  system(paste("cd", pkg$path, "; make"),
+  fileflag <- ifelse(makefile == "" || makefile=="Makefile" || makefile == "makefile", "", paste("-f", makefile))
+
+  system(paste("cd", pkg$path, "; make", fileflag),
          ignore.stdout=quiet, ignore.stderr=quiet,
          intern=FALSE)
 }
