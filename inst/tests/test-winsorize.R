caa4237a243bf2eff6a446aa0ca34364625136df
Karl Broman <kbroman@gmail.com>
2014-03-26 17:59:58 -0500

    Add to tests of winsorize

--- inst/tests/test-winsorize.R
+++ inst/tests/test-winsorize.R
@@ -3,10 +3,32 @@ context("winsorise")
 
 test_that("winsorize works for small vectors", {
 
-  x <-  c(2, 3, 7, 9, 6, NA, 5, 8, NA, 0, 4, 1, 10)
-  y1 <- c(2, 3, 7, 9, 6, NA, 5, 8, NA, 1, 4, 1,  9)
-  y2 <- c(2, 3, 7, 8, 6, NA, 5, 8, NA, 2, 4, 2,  8)
-  expect_identical(winsorize(x, 0.1), y1)
-  expect_identical(winsorize(x, 0.2), y2)
+  input <-   c(2, 3, 7, 9, 6, NA, 5, 8, NA, 0, 4, 1, 10)
+  result1 <- c(2, 3, 7, 9, 6, NA, 5, 8, NA, 1, 4, 1,  9)
+  result2 <- c(2, 3, 7, 8, 6, NA, 5, 8, NA, 2, 4, 2,  8)
+  expect_identical(winsorize(input, 0.1), result1)
+  expect_identical(winsorize(input, 0.2), result2)
+
+})
+
+test_that("winsorize works for a long vector", {
+
+  set.seed(94745689)
+  n <- 1000
+  nmis <- 10
+  p <- 0.05
+  input <- rnorm(n)
+  input[sample(1:n, nmis)] <- NA
+  quL <- quantile(input, p, na.rm=TRUE)
+  quH <- quantile(input, 1-p, na.rm=TRUE)
+
+  result <- winsorize(input, p)
+  expect_identical(is.na(input), is.na(result))
+  middle <- !is.na(input) & input >= quL & input <= quH
+  low <- !is.na(input) & input <= quL
+  high <- !is.na(input) & input >= quH
+  expect_identical(input[middle], result[middle])
+  expect_true( all(result[low] == quL) )
+  expect_true( all(result[high] == quH) )
 
 })
