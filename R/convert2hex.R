175b3734174c3a1a46b4a2eead335fe5af98b05e
Karl Broman <kbroman@gmail.com>
2014-05-07 11:31:50 -0500

    hex2dec and dec2hex: use built-in R functions

--- R/convert2hex.R
+++ R/convert2hex.R
@@ -31,32 +31,7 @@
 #' manip
 convert2hex <-
 function(d)
-{
-  if(length(d) > 1) {
-    res <- d
-    for(i in seq(along=d))
-      res[i] <- convert2hex(d[i])
-    return(res)
-  }
-
-  if(d != round(d) || d < 0)
-    stop("d must be a non-negative integer")
-
-  hex <- c(0:9,LETTERS[1:6])
-
-  if(d < 16) return(hex[d+1])
-
-  high <- floor(log(d, 16))
-
-  res <- ""
-  for(i in high:1) {
-    z <- d %/% (16^i)
-    res <- paste(res, hex[z + 1], sep="")
-    d <- d - z*(16^i)
-  }
-
-  paste(res, hex[(d %% 16) + 1], sep="")
-}
+  as.hexmode(d)
 
 #' @export
 dec2hex <- convert2hex
@@ -94,24 +69,7 @@ dec2hex <- convert2hex
 hex2dec <-
 function(h)
 {
-  if(length(h) > 1) {
-    res <- h
-    for(i in seq(along=h))
-      res[i] <- hex2dec(h[i])
-    return(res)
-  }
-
-  hex <- c(0:9,LETTERS[1:6])
-
-  if(!is.character(h)) h <- as.character(h)
-  h <- toupper(h)
-
-  hspl <- rev(unlist(strsplit(h, "")))
-  hc <- match(hspl, hex)-1
-  if(any(is.na(hc)))
-     stop("Invalid characters (", paste(hspl[is.na(hc)], collapse=" "), ") in ", h)
-
-  sum(hc * 16^(0:(length(hc)-1)))
+  strtoi(h, base=16)
 }
 
 # end of convert2hex.R
