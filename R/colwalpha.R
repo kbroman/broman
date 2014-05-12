bcefb2eb474112aaf484d29ee2d79bd18a15cf4c
Karl Broman <kbroman@gmail.com>
2014-05-12 09:51:11 -0500

    Add colwalpha function

---
+++ R/colwalpha.R
@@ -0,0 +1,23 @@
+## colwalpha
+##
+#' Convert a color to use alpha transparency
+#'
+#' Convert a color to RGB and then to RGB with alpha transparency
+#'
+#' @param color A character string for a color
+#' @param alpha Traparency value (between 0 and 1)
+#'
+#' @return A character string representing a color
+#' 
+#' @importFrom grDevices col2rgb rgb
+#' @export
+#' @keywords color
+colwalpha <-
+function(color, alpha=1)
+{
+  stopifnot(alpha >=0, alpha <= 1)
+  alpha <- alpha * 255
+
+  rgbval <- col2rgb(color)
+  rgb(rgbval[1], rgbval[2], rgbval[3], alpha=alpha, maxColorValue=255)
+}
