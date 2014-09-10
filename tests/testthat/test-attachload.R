a0ceb3e3d38fe7ac978c89f1ee3fbaa57325f673
Karl Broman <kbroman@gmail.com>
2014-09-10 06:43:08 -0500

    attachfile and loadfile: allow any number of padding 0's
     - deleted attachwork and loadwork functions as I never use them
     - Added some tests

---
+++ tests/testthat/test-attachload.R
@@ -0,0 +1,46 @@
+
+context("attachfile")
+
+test_that("attachfile and loadfile work", {
+
+    # create a file
+    z <- savedz <- "blah"
+    number <- 4
+    file <- paste0("perm000", number, ".RData")
+    save(z, file=file)
+    rm(z)
+
+    loadfile(number, verbose=TRUE)
+    expect_equal(z, savedz)
+    rm(z)
+
+    attachfile(number)
+    expect_equal(z, savedz)
+
+    detach(2)
+    unlink(file)
+
+})
+
+test_that("attachfile and loadfile work with a different filename", {
+
+    # create a file
+    z <- savedz <- "blah"
+    number <- 4
+    stem <- "blah"
+    end <- "_end.RData"
+    file <- paste0(stem, "000", number, end)
+    save(z, file=file)
+    rm(z)
+
+    loadfile(number, stem, end)
+    expect_equal(z, savedz)
+    rm(z)
+
+    attachfile(number, stem, end)
+    expect_equal(z, savedz)
+
+    detach(2)
+    unlink(file)
+})
+
