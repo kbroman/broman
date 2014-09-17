a63d68de2eb1659db51ee92ed16259138a3aa45b
Karl Broman <kbroman@gmail.com>
2014-09-16 22:22:35 -0500

    errors2pushbullet: include deviceind argument

--- R/errors2pushbullet.R
+++ R/errors2pushbullet.R
@@ -4,12 +4,15 @@
 #' Set options to use RPushbullet to use pushbullet to push
 #' notifications of any error messages.
 #'
+#' @param deviceind Numeric index for device to which errors will be
+#' pushed; passed to \code{\link[RPushbullet]{pbPost}}.
+#'
 #' @export
 #' @examples
 #' \dontrun{errors2pushbullet()}
 #' @keywords utilities
 errors2pushbullet <-
-function() {
+function(deviceind=1) {
     library(RPushbullet)
-    options(error = function() pbPost("note", "Error", geterrmessage()) )
+    options(error = function() pbPost("note", "Error", geterrmessage(), deviceind=deviceind) )
 }
