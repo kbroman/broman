3b9e0d4f52c5b3c02d1907b38820f3cc4acdc196
Karl Broman <kbroman@gmail.com>
2014-10-24 06:23:09 -0500

    Add func attrnames that just does names(attributes( obj ))

---
+++ R/attrnames.R
@@ -0,0 +1,18 @@
+#' Get names of attributes
+#'
+#' Get the names of the attributes of an object
+#'
+#' @param object Any object
+#'
+#' @return Vector of character strings with the names of the attributes.
+#'
+#' @details It just does \code{names(attributes(object))}.
+#'
+#' @export
+#' @examples
+#' x <- matrix(1:100, ncol=5)
+#' colnames(x) <- LETTERS[1:5]
+#' attrnames(x)
+
+attrnames <-
+    function(object) names(attributes(object))
