71dde8defa6be2550b64f14312d32861660e1b4b
Karl Broman <kbroman@gmail.com>
2015-01-27 09:10:40 -0600

    Incorporate .onAttach() code from RPushbullet
     - This way, RPushbullet is in Imports not Depends
     - Also add jsonlite to Depends

--- R/errors2pushbullet.R
+++ R/errors2pushbullet.R
@@ -11,12 +11,14 @@
 #' \code{\link[RPushbullet]{pbPost}}.)
 #'
 #' @export
-#' @import RPushbullet
+#' @import RPushbullet jsonlite
 #' @examples
 #' \dontrun{errors2pushbullet()}
 #' @keywords utilities
 errors2pushbullet <-
     function(recipients) {
+        load_pushbullet()
+
         if(!missing(recipients) && !is.null(recipients))
             options(error = function() {
                 RPushbullet::pbPost("note", "Error", geterrmessage(), recipients=recipients)
@@ -28,3 +30,41 @@ errors2pushbullet <-
                 if(!interactive()) stop(geterrmessage())
             } )
     }
+
+is_pushbullet_loaded <-
+    function()
+{
+    if(is.null(getOption("rpushbullet.key")))
+        return(FALSE)
+
+    TRUE
+}
+
+# the following is a modified version of code from
+#     https://github.com/eddelbuettel/rpushbullet/blob/master/R/init.R
+load_pushbullet <-
+    function()
+{
+    if(!is_pushbullet_loaded()) {
+        Sys.setenv(Rbroman_pushbullet_loaded = TRUE)
+
+        dotfile <- "~/.rpushbullet.json"
+        if (file.exists(dotfile)) {
+            message("Loading ", dotfile, " for RPushbullet")
+            pb <- jsonlite::fromJSON(dotfile)
+            assign("pb", pb, envir=new.env(parent=emptyenv()))
+            options("rpushbullet.key" = pb[["key"]])
+            options("rpushbullet.devices" = pb[["devices"]])
+            options("rpushbullet.names" = pb[["names"]])
+            ## defaultdevice is an optional entry, with fallback value of 0
+            options("rpushbullet.defaultdevice" =
+                    if ("defaultdevice" %in% names(pb)) pb[["defaultdevice"]] else 0)
+            ## these are for testing
+            options("rpushbullet.testemail" =
+                    if ("testemail" %in% names(pb)) pb[["testemail"]] else character())
+            options("rpushbullet.testchannel" =
+                    if ("testchannel" %in% names(pb)) pb[["testchannel"]] else character())
+        }
+        else stop("Cannot load ~/.rpushbullet.json")
+    }
+}
