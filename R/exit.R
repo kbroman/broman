11cd8ced7c20d980ab48207f56179e9f41eacddd
Karl Broman <kbroman@gmail.com>
2015-02-09 14:53:35 -0600

    Add exit() function, which is just q(no)

---
+++ R/exit.R
@@ -0,0 +1,12 @@
+#' exit R without saving
+#'
+#' exit R without saving workspace.
+#'
+#' @details This just calls \code{q("no")}
+#'
+#' @export
+#'
+#' @return None.
+#' @keywords utilities
+
+exit <- function() q("no")
