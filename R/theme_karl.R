acf8b91d26aa5a8e1b556f6939bf4be0447c45b4
Karl Broman <kbroman@gmail.com>
2015-04-22 12:14:30 -0500

    karl_theme now also as theme_karl
     - Also revised the definition to match the pattern used in defining
       theme_bw, namely take theme_gray() and then replace the differences

---
+++ R/theme_karl.R
@@ -0,0 +1,35 @@
+#' Karl's ggplot2 theme
+#'
+#' Karl's ggplot2 theme: black border and no ticks
+#'
+#' @param base_size Base font size
+#' @param base_family Base font family
+#' @param ... Passed to \code{\link[ggplot2]{theme}}
+#'
+#' @return An object as returned by \code{\link[ggplot2]{theme}}
+#'
+#'
+#' @examples
+#' library(ggplot2)
+#' mtcars$cyl <- factor(mtcars$cyl)
+#' ggplot(mtcars, aes(y=mpg, x=disp, color=cyl)) +
+#'     geom_point() + theme_karl()
+#'
+#' @seealso \code{\link[ggplot2]{theme}}
+#'
+#' @export
+theme_karl <-
+    function(base_size = 12, base_family = "", ...)
+{
+    ggplot2::"%+replace%"(
+        ggplot2::theme_grey(base_size = base_size, base_family = base_family) ,
+
+        ggplot2::theme(axis.ticks.length=grid::unit(0, "cm"),
+                       panel.border=ggplot2::element_rect(fill=NA, color="black"),
+                       ...)
+        )
+}
+
+#' @rdname theme_karl
+#' @export
+karl_theme <- theme_karl
