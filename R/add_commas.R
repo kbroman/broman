3925330292f08e1576048dc6a36b0fcd8efc3754
Karl Broman <kbroman@gmail.com>
2015-07-16 18:43:25 -0500

    Simplify add_commas() by using a call to format()

--- R/add_commas.R
+++ R/add_commas.R
@@ -10,20 +10,9 @@
 #' @return Character string with numbers written like \code{"7,547,085"}.
 #'
 #' @examples
-#' add_commas(c(231.3, 91310.2, 2123.8, 9911001020, 999723285))
+#' add_commas(c(231, 91310, 2123, 9911001020, 999723285))
 add_commas <-
     function(numbers)
 {
-    str <- as.character(round(as.numeric(numbers)))
-    spl <- strsplit(str, "")
-    spl <- lapply(spl, rev)
-    spl <- lapply(spl, function(a) {
-        b <- NULL
-        for(i in seq(1, length(a), by=3)) {
-            if(!is.null(b)) b <- c(b, ",")
-            b <- c(b, a[i:min(c(i+2, length(a)))])
-        }
-        paste(rev(b), collapse="") })
-
-    unlist(spl)
+    format(numbers, big.mark=",", scientific=FALSE, trim=TRUE)
 }
--- tests/testthat/test-add_commas.R
+++ tests/testthat/test-add_commas.R
@@ -3,7 +3,10 @@ context("add_commas")
 test_that("add_commas works", {
 
     input <- c(231.3, 91310.2, 2123.8, 9911001020, 999723285)
-    output <- c("231", "91,310", "2,124", "9,911,001,020", "999,723,285")
+    output <- c("231.3", "91,310.2", "2,123.8", "9,911,001,020.0", "999,723,285.0")
     expect_equal(add_commas(input), output)
+
+    output <- c("231", "91,310", "2,124", "9,911,001,020", "999,723,285")
+    expect_equal(add_commas(round(input)), output)
 
 })
