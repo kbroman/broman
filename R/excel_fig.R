20d4e567b34f2f592e80efa757e749c72dc1c745
Karl Broman <kbroman@gmail.com>
2015-08-01 19:01:04 -0500

    Revise excel_fig() to allow highlighting of some cells

--- R/excel_fig.R
+++ R/excel_fig.R
@@ -12,6 +12,8 @@
 #' @param headborder Color of border of cells on the top and left border
 #' @param row_names If TRUE, and row names are present, include them as a first column
 #' @param col_names If TRUE, and column names are present, include them as a first row
+#' @param hilitcells Character vector of cells to highlight, like \code{"A1"} or \code{"D4"}
+#' @param hilitcolor Color to highlight cells, a vector of length 1 or the same length as \code{hilitcells}
 #'
 #' @export
 #' @importFrom grDevices svg png jpeg pdf dev.off
@@ -28,7 +30,8 @@
 excel_fig <-
     function(mat, file, cellwidth=100, cellheight=20, pointsize=14,
              border="#CECECE", headcol="#E9E9E9", headborder="#969696",
-             row_names=FALSE, col_names=TRUE)
+             row_names=FALSE, col_names=TRUE,
+             hilitcells, hilitcolor="#F0DCDB")
 
 {
     headtextcol <- "#626262"
@@ -62,12 +65,57 @@ excel_fig <-
     plot(0,0,type="n", xlab="", ylab="", xaxt="n", yaxt="n",
          xlim=c(0, width), ylim=c(height, 0), xaxs="i", yaxs="i")
 
+    # matrix containing color of cells
+    colormat <- matrix("white", nrow=nrow(mat), ncol=ncol(mat))
+
+    if(!missing(hilitcells) && !is.null(hilitcells)) {
+
+        # make sure hilitcolor is a vector of same length as hilitcells
+        if(length(hilitcolor) == 1)
+            hilitcolor <- rep(hilitcolor, length(hilitcells))
+        else if(length(hilitcolor) != length(hilitcells))
+            stop("length(hilitcolor) (", length(hilitcolor),
+                 ") != length(hilitcells) (", length(hilitcells), ")")
+
+        hilitcells <- toupper(hilitcells) # to upper-case
+        hilitcells <- strsplit(hilitcells, "") # split into characters
+
+        # check that cells are valid ("A4" "D05", etc)
+        sapply(hilitcells, function(a) {
+               let <- which(a %in% LETTERS)
+               num <- which(!(a %in% LETTERS))
+               if(length(let) == 0 || length(num) == 0 ||
+                  any(sapply(let, function(a,b) any(a > b), num)))
+                   stop("Invalid highlighted cell: ", paste(a, collapse=""))
+           })
+
+
+        # pull out letters and convert to column number
+        col <- vapply(hilitcells, function(a) {
+            b <- a[a %in% LETTERS] # pull out the letters
+            m <- match(b, LETTERS) # convert to digits
+            if(length(b) == 1) return(m) # if 1 letter, just use corresponding number
+            sum((26^((length(b)-1):0))*m) # otherwise, treat like base 26, offset by 26
+        }, 1)
+
+        # pull out the digits and convert back to row number
+        row <- vapply(hilitcells, function(a) as.numeric(paste(a[!(a %in% LETTERS)], collapse="")), 1)
+
+        # make a matrix
+        hilitcells <- cbind(row, col)
+
+        # change color of highlighted cells
+        for(i in 1:nrow(hilitcells))
+            colormat[hilitcells[i,1], hilitcells[i,2]] <- hilitcolor[i]
+    }
+
+
     for(i in n_row:0) {
         for(j in n_col:0) {
             # rectangles
             rect(j*cellwidth, i*cellheight, (j+1)*cellwidth, (i+1)*cellheight,
                  border=ifelse(i==0 || j==0, headborder, border),
-                 col=ifelse(i==0 || j==0, headcol, "white"))
+                 col=ifelse(i==0 || j==0, headcol, colormat[i,j]))
 
             # text
             if(i==0 && j>0)
