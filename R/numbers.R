39ba9960256179bce9c7986604b1fee02a1c6f4a
Karl Broman <kbroman@gmail.com>
2016-06-20 10:28:08 -0500

    Add data objects numbers and Numbers

---
+++ R/numbers.R
@@ -0,0 +1,28 @@
+#' Numbers spelled out in English
+#'
+#' The numbers 1-20 spelled out in English, for use in reports.
+#'
+#' @name numbers
+#'
+#' @docType data
+#'
+#' @aliases Numbers
+#'
+#' @details
+#' \itemize{
+#' \item \code{numbers} - lower case
+#' \item \code{Numbers} - Capitalized
+#' }
+#'
+#' @format A vector of character strings
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' numbers[5]
+#' Numbers[5]
+NULL
+
+#' @name Numbers
+#' @rdname numbers
+NULL
