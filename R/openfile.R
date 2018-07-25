0a15de72c24307c8ca6ad9126c42b2ccb41ff9d6
Karl Broman <kbroman@gmail.com>
2018-07-24 21:36:46 -0500

    Revise openfile() to work on linux

--- R/openfile.R
+++ R/openfile.R
@@ -2,14 +2,17 @@
 #'
 #' Open a file
 #'
-#' Open a file using \code{\link[base]{system}} and \code{open}.
+#' Open a file using \code{\link[base]{system}} and \code{"open"}
+#' (well, actually \code{"start"} on Linux).
 #'
 #' @param file File name (character string)
 #'
 #' @details I'd thought that to open a file you'd use \code{open} in
-#' MacOS and \code{start} in Windows, but \code{system("start
-#' myfile.pdf")} doesn't work in Windows, but rather
-#' \code{system("open myfile.pdf")} does, so here we're just using
+#'     MacOS and \code{start} in Windows, but
+#'     \code{system("start myfile.pdf")} doesn't work in Windows, and
+#'     rather \code{system("open myfile.pdf")} does, so here we're
+#'     just using \code{open}, except on Linux where at least on my
+#'     system, you can use \code{"start"}.
 #'
 #' @export
 #' @return None.
@@ -22,5 +25,10 @@
 openfile <-
     function(file)
 {
-    system( paste("open", file) )
+    # determine OS
+    os <- Sys.info()["sysname"]
+
+    command <- ifelse(os=="Linux", "start", "open")
+
+    system( paste(command, file) )
 }
