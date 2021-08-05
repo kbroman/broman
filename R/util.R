84b4c432faf63da3d109e303684574a7ea1a9a75
Karl Broman <kbroman@gmail.com>
2021-08-05 15:58:07 -0500

    Revise bromanversion() to handle case like "0.74"

--- R/util.R
+++ R/util.R
@@ -22,6 +22,10 @@ bromanversion <-
 {
     version <- unlist(utils::packageVersion("broman"))
 
-    # make it like #.#-#
-    paste(c(version,".","-")[c(1,4,2,5,3)], collapse="")
+    if(length(version) == 3) {
+        # make it like #.#-#
+        return( paste(c(version, ".", "-")[c(1,4,2,5,3)], collapse="") )
+    }
+
+    paste(version, collapse=".")
 }
