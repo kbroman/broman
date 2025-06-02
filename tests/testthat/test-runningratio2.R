1f16a57e48672f3154740ac1638ea8c216e9af63
Karl Broman <kbroman@gmail.com>
2025-06-02 17:37:52 -0500

    Add more tests of runningratio and runningratio2

--- tests/testthat/test-runningratio2.R
+++ tests/testthat/test-runningratio2.R
@@ -28,3 +28,25 @@ test_that("runningratio2 with constant x or position", {
   expect_equal( runningratio2(pos, x, denom, window_denom=1000), x/d)
 
 })
+
+
+test_that("runningratio and runningratio2 match when equally-spaced positions", {
+
+  n <- 100
+  d <- 5
+  x <- rnorm(n)
+  pos <- 1:n
+  denom <- rep(d, n)
+
+  expect_equal( runningratio(pos, x, denom, window=1.1), x/d)
+  expect_equal( runningratio2(pos, x, denom, window_denom=d-1), x/d)
+
+  result <- c(mean(x[1:2]), rowMeans(cbind(x[-((n-1):n)], x[-c(1,n)], x[-(1:2)])), mean(x[(n-1):n]))/d
+  expect_equal( runningratio(pos, x, denom, window=2.1), result)
+
+  result[1] <- result[2]
+  result[n] <- result[n-1]
+
+  expect_equal( runningratio2(pos, x, denom, window_denom=d*2+1), result)
+
+})
